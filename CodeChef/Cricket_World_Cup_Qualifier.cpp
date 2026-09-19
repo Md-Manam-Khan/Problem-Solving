2ce58bc7303153a9c3583fd08d27b279bf7efb3b
Md-Manam-Khan <manamkhan921@gmail.com>
2026-09-20 04:08:32 +0600

    Codechef Problems solved

---
+++ CodeChef/Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace  std;
+using ll = long long;
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll x;
+    cin>>x;
+    if (x >= 12)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
