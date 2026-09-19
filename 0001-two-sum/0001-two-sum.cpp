c0fd283b5fb6f520f83f7617c5f30738d07b6019
Md. Manam Khan <manamkhan921@gmail.com>
2026-09-19 22:08:23 +0600

    Time: 140 ms (5.02%), Space: 14.2 MB (62.77%) - LeetHub - Time: O(n^2), Space: O(1)

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,19 +4,19 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
         int i, x, n, j;
-        vector <int> a;
+        vector <int> num;
         for (i = 0; i < nums.size() - 1; i++)
         {
             for (j = i + 1; j < nums.size(); j++)
             {
                 if ((nums[i] + nums[j]) == target)
                 {
-                    a.push_back(i);
-                    a.push_back(j);
+                    num.push_back(i);
+                    num.push_back(j);
                 }
             }
         }
-        return a;
+        return num;
     }
 };
 
