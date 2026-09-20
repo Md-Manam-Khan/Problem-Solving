85c8f890eb1434be3d3ef3bc4ccb8d1f31737e56
Md-Manam-Khan <manamkhan921@gmail.com>
2026-09-21 00:39:54 +0600

    New CodeChef problems solved

---
+++ CodeChef/-500/Biryani_classes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace  std;
+using ll = long long;
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll x, t, y;
+    cin>>t;
+    while (t--)
+    {
+        cin>>x>>y;
+        cout<< x * y <<"\n";
+    }
+}
---
+++ CodeChef/-500/Chef_Plays_Ludo.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace  std;
+using ll = long long;
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll x, t;
+    cin>>t;
+    while (t--)
+    {
+        cin>>x;
+        if (x == 6)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+}
