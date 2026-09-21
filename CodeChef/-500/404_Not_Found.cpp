35c2c449c91de3016fb765b1bf5caf9209960588
Md-Manam-Khan <manamkhan921@gmail.com>
2026-09-22 00:06:59 +0600

    New CodeChef problem solved

---
+++ CodeChef/-500/404_Not_Found.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace  std;
+using ll = long long;
+int main ()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll x, t, y;
+    cin>>t;
+    if (t == 404)
+    {
+        cout<<"NOT FOUND";
+    }
+    else
+    {
+        cout<<"FOUND";
+    }
+}
